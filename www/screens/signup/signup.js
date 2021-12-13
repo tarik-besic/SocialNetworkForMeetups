9c835b02606cc3ac6e98e0781b24c154543ae691
Tarik Besic <tarik_besic@live.com>
2021-12-13 23:52:51 +0100

    added new screen

---
+++ www/screens/signup/signup.js
@@ -0,0 +1,11 @@
+class signupScreen extends Screen {
+    static Name = 'signup Screen'
+    static Id = 'signup-screen';
+    static Template = '';
+
+    constructor(){
+        super();
+    }
+}
+
+app.screens['signup'] = signupScreen;

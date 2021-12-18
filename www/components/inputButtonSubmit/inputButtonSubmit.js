e033afe733a77f3f5898af7ddb4fa0edbf8536ca
Tarik Besic <tarik_besic@live.com>
2021-12-18 13:29:26 +0100

    added dynamic data for button text from screen

--- www/components/inputButtonSubmit/inputButtonSubmit.js
+++ www/components/inputButtonSubmit/inputButtonSubmit.js
@@ -3,7 +3,10 @@ class inputButtonSubmitComponent extends BaseComponent{
     static ClassName = 'component-inputButtonSubmit';
 
     static init_events(root_dom){
-        
+
+        let data=root_dom.dataset.value; //getting data
+        root_dom.childNodes[0].firstChild.nextSibling.innerHTML=data //setting data
+
     }
 }
 

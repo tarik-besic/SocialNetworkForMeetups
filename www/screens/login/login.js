da6a94421fd907e343ebdd66424ac31fbbc6649d
Tarik Besic <tarik_besic@live.com>
2021-12-18 13:27:56 +0100

    made new screen

---
+++ www/screens/login/login.js
@@ -0,0 +1,11 @@
+class loginScreen extends Screen {
+    static Name = 'login Screen'
+    static Id = 'login-screen';
+    static Template = '';
+
+    constructor(){
+        super();
+    }
+}
+
+app.screens['login'] = loginScreen;

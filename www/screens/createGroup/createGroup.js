e4538fc93e5e029e71b60ee8766c750a2dc0c2ed
Tarik Besic <tarik_besic@live.com>
2021-12-21 00:31:20 +0100

    new screen

---
+++ www/screens/createGroup/createGroup.js
@@ -0,0 +1,11 @@
+class createGroupScreen extends Screen {
+    static Name = 'createGroup Screen'
+    static Id = 'createGroup-screen';
+    static Template = '';
+
+    constructor(){
+        super();
+    }
+}
+
+app.screens['createGroup'] = createGroupScreen;
